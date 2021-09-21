4176618bf89a05383216b9a23158ee4e5c613bb1
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-09-21 06:13:19 +0530

    Added: To Check zerofeet with onefeet for equality

--- src/test/java/quantitymeasurement/QuantityTest.java
+++ src/test/java/quantitymeasurement/QuantityTest.java
@@ -11,4 +11,11 @@ public class QuantityTest {
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1, feet2);
     }
+
+    @Test
+    public void givenZeroFeetAndOneFeet_WhenEquals_ThenShouldBeEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1, feet2);
+    }
 }
